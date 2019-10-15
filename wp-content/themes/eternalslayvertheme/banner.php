9668411cc469fe5005f5817eacfc0c329cd93002
Nacho RM <joseignaciorm13@gmail.com>
2019-10-15 20:46:19 +0200

    Nueva linea irrelevante, banner

--- wp-content/themes/eternalslayvertheme/banner.php
+++ wp-content/themes/eternalslayvertheme/banner.php
@@ -1,6 +1,7 @@
 <!-- BANNER SECTION -->
 <section class="main_banner">
 <div class="main_banner__slider" id="bannerSlider">
+
 </div>
 <div class="main_banner__slider--slider-btns">
 <div class="slider-btn" id="prev">&lt;</div>
